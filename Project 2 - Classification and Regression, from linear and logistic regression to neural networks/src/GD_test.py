247640ad5fdc28f05dc8ad4ac8dcd0a6e1a99f06
JouvalSomer <jouval.somer@gmail.com>
2022-10-25 13:15:06 +0200

    Working GD

--- Project 2 - Classification and Regression, from linear and logistic regression to neural networks/src/GD_test.py
+++ Project 2 - Classification and Regression, from linear and logistic regression to neural networks/src/GD_test.py
@@ -5,22 +5,31 @@ import matplotlib.pyplot as plt
 from autograd import grad
 from sklearn.preprocessing import StandardScaler
 
-def CostOLS(beta, X, y):
+def CostOLS(theta, X, y):
     m = X.shape[0]
-    return (1.0/m)*np.sum((y - (X @ beta))**2)
+    return (1.0/m)*np.sum((y - (X @ theta))**2)
 
-def gradient_CostOLS(X, y, beta):
+def gradient_CostOLS(theta, X, y):
     m = X.shape[0]
-    grad = 2/m * X.T @ (X @ beta - y)
+    grad = 2/m * X.T @ (X @ theta - y)
     return np.mean(grad, axis=1)
+
+def auto_gradient(theta, X, y):
+    training_gradient = grad(CostOLS)
+    return training_gradient(theta, X, y)
+
+def learning_schedule(iter, init_LR, decay):
+    return init_LR * 1/(1 + decay*iter)
 
 def scaling(X):
     scaler = StandardScaler()
     scaler.fit(X)
     X = scaler.transform(X)
+    X[:,0] = 1
     return X
 
-def GD(method, Niterations, iterprint, plot):
+def GD(method, Niterations, iterprint, init_LR, decay, plot):
+
     """ Design matrix and OLS """
     X = np.c_[np.ones((n,1)), x, x**2]
     XT_X = X.T @ X
@@ -30,26 +39,26 @@ def GD(method, Niterations, iterprint, plot):
 
     """ Scaling """
     # X = scaling(X)
-    # X[:,0] = 1
 
     """ Gradient decent """
     theta = np.random.randn(3,1) # Initial thetas/betas
-    eta = 0.1 # Learning rate
-
-    # Define the gradient:
-    if method == 'auto':
-        training_gradient = grad(CostOLS) # Autograd of the cost function 
 
     # Gradient decent:
     for iter in range(Niterations):
+
         if method == 'auto':
-            gradients = training_gradient(theta, X, y) # Autograd
-        else:
-            gradients = gradient_CostOLS(X, y, theta).reshape(-1, 1)
+            gradients =  auto_gradient(theta, X, y) # Autograd
+        if method == 'anal':
+            gradients = gradient_CostOLS(theta, X, y) # Analytical 
+
+        eta = learning_schedule(iter, init_LR, decay)
+
         if iter % iterprint == 0: # Print theta and gradient every 50th iteration
             print('iter = ', iter)
             print('theta = \n', theta)
             print('gradients = \n', gradients, '\n')
+            print('eta = ', eta)
+
         theta -= eta*gradients
 
     y_predict_OLS = X @ theta_linreg 
@@ -57,7 +66,7 @@ def GD(method, Niterations, iterprint, plot):
 
     if plot:
         plt.plot(x, y_predict_OLS, "b-", label='OLS')
-        plt.plot(x, y_predict_GD, "r-", label='GD')
+        plt.plot(x, y_predict_GD, "r-", label='GD') 
         plt.plot(x, y ,'g.', label='Data') # Data
         plt.xlabel(r'$x$')
         plt.ylabel(r'$y$')
@@ -68,17 +77,21 @@ def GD(method, Niterations, iterprint, plot):
 if __name__=='__main__':
     """ Data """
     n = 15
-    x = np.linspace(0, 2, n)
+    x = np.linspace(0, 1, n)
     y = 3 + 2*x + 3*x**2
     alpha = 0.1 # Noise scaling
     np.random.seed(3155)
-    y = y + alpha*np.random.normal(0, 1, x.shape)
+    y = (y + alpha*np.random.normal(0, 1, x.shape)).reshape(-1, 1)
+
+    
+    init_LR = 0.01 # Initial learning rate
+    decay = 0.001 # Learning rate decay rate
 
     """ Gradient method """
     method = 'auto'
-    # method = 'manually'
+    # method = 'anal'
 
     plot = True
-    Niterations = 100 # Number of GD iterations
+    Niterations = 400 # Number of GD iterations
     iterprint = 10 # Print theta and gradients every iterprint-th iteration
-    GD(method, Niterations, iterprint, plot)
+    GD(method, Niterations, iterprint, init_LR, decay, plot)
