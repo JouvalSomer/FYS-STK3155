9726f25c828929e861b5b430a255ba91ed73adcd
JouvalSomer <jouval.somer@gmail.com>
2022-10-26 16:04:37 +0200

    Now with MSE plots for GD and SGD ++

--- Project 2 - Classification and Regression, from linear and logistic regression to neural networks/src/GD_and_SGD.py
+++ Project 2 - Classification and Regression, from linear and logistic regression to neural networks/src/GD_and_SGD.py
@@ -4,6 +4,36 @@ import matplotlib.pyplot as plt
 # from autograd import elementwise_grad as egrad
 from autograd import grad
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+
+def FrankeFunction(x,y, noice, alpha, seed):
+    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
+    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
+    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
+    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+    if noice:
+        np.random.seed(seed)
+        return term1 + term2 + term3 + term4 + alpha*np.random.normal(0, 1, x.shape)
+    else:
+        return term1 + term2 + term3 + term4
+
+def data_FF(noise=True, step_size=0.05, alpha=0.05):
+    x = np.arange(0, 1, step_size)
+    y = np.arange(0, 1, step_size)
+
+    X, Y = np.meshgrid(x, y)
+    x = X.flatten().reshape(-1, 1)
+    y = Y.flatten().reshape(-1, 1)
+    Z = FrankeFunction(X, Y, noise, alpha, seed=3155)
+    z = Z.flatten().reshape(-1, 1)
+    return x, y, z
+
+def MSE(z, z_tilde):
+    return mean_squared_error(z, z_tilde)
+
+def r2(z, z_tilde):
+    return r2_score(z, z_tilde)
 
 def scaling(X):
     scaler = StandardScaler()
@@ -11,6 +41,19 @@ def scaling(X):
     X = scaler.transform(X)
     X[:,0] = 1
     return X
+
+def designMatrix(x, polygrad):
+    n = len(x) 
+    X = np.ones((n,polygrad)) 
+    for i in range(1,polygrad):
+        X[:,i] = (x**i).ravel()
+    return X
+
+def y_func(x, exact_theta):
+    y = 0
+    for i, theta in enumerate(exact_theta):
+        y += theta*x**i
+    return y
 
 def CostOLS(theta, X, y):
     m = X.shape[0]
@@ -89,14 +132,36 @@ def OLS(X, y):
     print("OLS thetas:")
     print(theta_linreg, '\n')
     y_predict_OLS = X @ theta_linreg 
-    return y_predict_OLS
+    return y_predict_OLS, theta_linreg
+
+def RIDGE(x_train, scale, lambda_min, lambda_max, nlambdas):
         
+    lambdas = np.logspace(lambda_min, lambda_max, nlambdas)
+
+    for degree in range(order):
+
+        D_M_train = designMatrix(x_train, degree)
+        D_M_test = designMatrix(x_train, degree)
+        if scale:
+            """Scaling the design matrices based on train matrix"""
+            D_M_train, D_M_test = scaling(D_M_train, D_M_test)
+
+        for l in range(nlambdas):
+            lmb = lambdas[l]
+
+            I = np.eye(np.shape(D_M_train)[1],np.shape(D_M_train)[1])
+
+            Ridgebeta = np.linalg.inv(D_M_train.T @ D_M_train+lmb*I) @ D_M_train.T @ z_train
+
+            z_tilde_test = D_M_test @ Ridgebeta
+       
 def GD(X, y, method, Niterations, init_LR, decay, momentum, seed):
 
     """ Gradient Decent """
     np.random.seed(seed)
     theta = np.random.randn(np.shape(X)[1],1) # Initial thetas/betas
     change = 0.0
+    mse = np.zeros(Niterations)
     # Gradient decent:
     for i in range(Niterations):
 
@@ -108,28 +173,32 @@ def GD(X, y, method, Niterations, init_LR, decay, momentum, seed):
         eta = learning_schedule(i, init_LR, decay) # LR
         update = eta * gradients + momentum * change # Update to the thetas
         theta -= update # Updating the thetas
+        
+        y_predict_GD_test = X_test @ theta
+        mse[i] = MSE(y_test, y_predict_GD_test)
         change = update # Update the amount the momentum gets added 
 
     print('GD thetas:')
     print(theta, '\n')
 
     y_predict_GD = X @ theta
-    return y_predict_GD
+    return y_predict_GD, theta, mse
 
-def SGD(X, y, Optimizer_method, Gradient_method, minibatch_size, n_epochs, init_LR, decay, momentum, seed):
+def SGD(X, y, Optimizer_method, Gradient_method, minibatch_size, n_minibatches, n_epochs, init_LR, decay, momentum, seed):
 
     """ Stochastic Gradient Decent """
     np.random.seed(seed)
     theta = np.random.randn(np.shape(X)[1],1) # Initial thetas/betas
     change = 0.0
 
-    n_minibatches = int(n/minibatch_size) #number of minibatches
 
     np.random.seed(seed)
     random_index = minibatch_size*np.random.randint(n_minibatches)
     X_batch = X[random_index:random_index + minibatch_size]
     y_batch = y[random_index:random_index + minibatch_size]
-
+    
+    mse = np.zeros(n_epochs*n_minibatches)
+    count = 0
     for epoch in range(n_epochs):
         Giter = np.zeros(shape=(3,3))
         for batch in range(n_minibatches):
@@ -158,51 +227,89 @@ def SGD(X, y, Optimizer_method, Gradient_method, minibatch_size, n_epochs, init_
                 theta -= update
                 change = update
 
+            y_predict_GD_test = X_test @ theta
+            mse[count] = MSE(y_test, y_predict_GD_test)
+            count += 1
+
     print('SGD thetas:')
     print(theta, '\n')
 
     y_predict_SGD = X @ theta
-    return y_predict_SGD
+    return y_predict_SGD, theta, mse
 
 
 if __name__=='__main__':
+
     """ Data """
-    n = 100
+    n = 400
     x = np.linspace(0, 1, n)
-    y = 3 + 2*x + 3*x**2
     exact_theta = [3.0, 2.0, 3.0]
     print('Exact theta:\n', np.array(exact_theta).reshape(-1, 1), '\n')
     alpha = 0.5 # Noise scaling
     seed = 3155
+    y = y_func(x, exact_theta)
     np.random.seed(seed)
     y = (y + alpha*np.random.normal(0, 1, x.shape)).reshape(-1, 1)
 
-    # Design matrix
-    X = np.c_[np.ones((n,1)), x, x**2]
 
-    n_epochs = 20 #number of epochs
+
+    """ Train Test Split """
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state = seed)
+    polygrad = len(exact_theta)
+    X_train = designMatrix(x_train, polygrad)
+    X_test = designMatrix(x_test, polygrad)
+
+
+    """ Hyperparameters """
+    n_epochs = 40 #number of epochs
     init_LR = 0.01 # Initial learning rate (LR)
     decay = init_LR/n_epochs # LR decay rate (for fixed LR set decay to 0)
     momentum = 0.9 # Momentum value
-    minibatch_size = 10
-
+    minibatch_size = 40
+    n_minibatches = int(np.shape(X_train)[0]/minibatch_size) #number of minibatches
+    Niterations = 400 # Number of GD iterations
 
     """ Gradient method """
     Gradient_method = ['auto', 'anal']
 
+    """ Optimization method """
     # If you want plain GD without any optimization choose 'momentum' with momentum value of 0
     Optimizer_method = ['Adagrad', 'RMSprop', 'momentum']
 
-    Niterations = 400 # Number of GD iterations
-    y_predict_GD = GD(X, y, Gradient_method[0], Niterations, init_LR, decay, momentum, seed)
-    y_predict_SGD = SGD(X, y, Optimizer_method[2], Gradient_method[0], minibatch_size, n_epochs, init_LR, decay, momentum, seed)
-    y_predict_OLS = OLS(X, y)
 
-    plt.plot(x, y_predict_GD, "r-", label='GD') 
-    plt.plot(x, y_predict_SGD, "b-", label='SGD') 
-    plt.plot(x, y_predict_OLS, "k-", zorder=100, label='OLS') 
-    plt.plot(x, y ,'g.', label='Data') # Data
+
+    """ Results """
+    y_predict_GD, theta_GD, mse_GD = GD(X_train, y_train, Gradient_method[0], Niterations, init_LR, decay, momentum, seed)
+    y_predict_SGD, theta_SGD, mse_SGD = SGD(X_train, y_train, Optimizer_method[2], Gradient_method[0], minibatch_size, n_minibatches, n_epochs, init_LR, decay, momentum, seed)
+    y_predict_OLS, theta_OLS = OLS(X_train, y_train)
+
+    y_predict_GD_test = X_test @ theta_GD
+    print(f'MSE_GD  = {MSE(y_test, y_predict_GD_test):.5f}')
+
+    y_predict_SGD_test = X_test @ theta_SGD
+    print(f'MSE_SGD = {MSE(y_test, y_predict_SGD_test):.5f}')
+
+    y_predict_OLS_test = X_test @ theta_OLS
+    print(f'MSE_OLS = {MSE(y_test, y_predict_OLS_test):.5f}')
+
+
+    """ Regression line plot """
+    plt.scatter(x_train, y_predict_GD, c='r', label='GD') 
+    plt.scatter(x_train, y_predict_SGD, c='b', label='SGD') 
+    plt.scatter(x_train, y_predict_OLS, c='k', zorder=100, label='OLS') 
+    plt.plot(x_train, y_train, 'g.', label='Data') # Data
     plt.xlabel(r'$x$')
     plt.ylabel(r'$y$')
+    plt.legend()
+    plt.show()
+    
+
+    """ MSE plot """
+    plt.yscale('log')
+    iter_max = min(Niterations, n_epochs*n_minibatches)
+    plt.plot(np.arange(Niterations)[:iter_max], mse_GD[:iter_max], label='MSE for GD')
+    plt.plot(range(n_epochs*n_minibatches)[:iter_max], mse_SGD[:iter_max], label='MSE for SGD')
+    plt.xlabel(r'$Iterations$')
+    plt.ylabel(r'$MSE$')
     plt.legend()
     plt.show()
